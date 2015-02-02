e7048517e7a37b8222400c9747af41195da4aa94
Sung Hyun (Jason) Yoon <jasony0206@gmail.com>
2015-02-01 19:16:16 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,571 @@
+#include <iostream>
+#include <cstdlib>  // atoi
+#include <stack>
+#include <climits>
+#include "Maze.h"
+#include "MazeDefinitions.h"
+#include "PathFinder.h"
+using namespace std;
+
+const int SIZE = 16;
+
+/**
+* Demo of a PathFinder implementation.
+*
+* Do not use a left/right wall following algorithm, as most
+* Micromouse mazes are designed for such algorithms to fail.
+*/
+
+/*
+class LeftWallFollower : public PathFinder {
+public:
+LeftWallFollower(bool shouldPause = false) : pause(shouldPause) {
+shouldGoForward = false;
+visitedStart = false;
+}
+
+MouseMovement nextMovement(unsigned x, unsigned y, const Maze &maze) {
+const bool frontWall = maze.wallInFront();
+const bool leftWall  = maze.wallOnLeft();
+
+// Pause at each cell if the user requests it.
+// It allows for better viewing on command line.
+if(pause) {
+std::cout << "Hit enter to continue..." << std::endl;
+std::cin.ignore(10000, '\n');
+std::cin.clear();
+}
+
+std::cout << maze.draw(5) << std::endl << std::endl;
+
+// If we somehow miraculously hit the center
+// of the maze, just terminate and celebrate!
+if(isAtCenter(x, y)) {
+std::cout << "Found center! Good enough for the demo, won't try to get back." << std::endl;
+return Finish;
+}
+
+// If we hit the start of the maze a second time, then
+// we couldn't find the center and never will...
+if(x == 0 && y == 0) {
+if(visitedStart) {
+std::cout << "Unable to find center, giving up." << std::endl;
+return Finish;
+} else {
+visitedStart = true;
+}
+}
+
+// If we have just turned left, we should take that path!
+if(!frontWall && shouldGoForward) {
+shouldGoForward = false;
+return MoveForward;
+}
+
+// As long as nothing is in front and we have
+// a wall to our left, keep going forward!
+if(!frontWall && leftWall) {
+shouldGoForward = false;
+return MoveForward;
+}
+
+// If our forward and left paths are blocked
+// we should try going to the right!
+if(frontWall && leftWall) {
+shouldGoForward = false;
+return TurnClockwise;
+}
+
+// Lastly, if there is no left wall we should take that path!
+if(!leftWall) {
+shouldGoForward = true;
+return TurnCounterClockwise;
+}
+
+// If we get stuck somehow, just terminate.
+std::cout << "Got stuck..." << std::endl;
+return Finish;
+}
+
+protected:
+// Helps us determine that we should go forward if we have just turned left.
+bool shouldGoForward;
+
+// Helps us determine if we've made a loop around the maze without finding the center.
+bool visitedStart;
+
+// Indicates we should pause before moving to next cell.
+// Useful for command line usage.
+const bool pause;
+
+bool isAtCenter(unsigned x, unsigned y) const {
+unsigned midpoint = MazeDefinitions::MAZE_LEN / 2;
+
+if(MazeDefinitions::MAZE_LEN % 2 != 0) {
+return x == midpoint && y == midpoint;
+}
+
+return  (x == midpoint     && y == midpoint    ) ||
+(x == midpoint - 1 && y == midpoint    ) ||
+(x == midpoint     && y == midpoint - 1) ||
+(x == midpoint - 1 && y == midpoint - 1);
+}
+};
+*/
+
+/////////////
+//Floodfill//
+/////////////
+
+const int N = 1;
+const int E = 2;
+const int S = 3;
+const int W = 0;
+
+struct wall{
+	bool north;
+	bool south;
+	bool east;
+	bool west;
+};
+
+//upper left corner = (0,0)
+struct coord{
+	coord(unsigned sx, unsigned sy){
+		x = sx;
+		y = sy;
+	}
+	unsigned x;
+	unsigned y;
+};
+
+int orient = 0;
+unsigned char dist[16][16];
+wall walls[SIZE][SIZE];
+
+
+//void setBit(int a[], int k)
+void printArray(unsigned char dist[][SIZE]){
+	for(int i=0; i<SIZE; i++){
+		for(int j=0; j<SIZE; j++){
+			cout << (int)dist[i][j]<<'\t';
+		}
+		cout << endl;
+	}
+}
+
+class Floodfill : public PathFinder {
+public:
+	Floodfill(bool shouldPause = false) : pause(shouldPause) {
+		orient = N;
+		shouldGoForward = false;
+		visitedStart = false;
+		//initialize dist 2d array
+		for(int i=0; i<SIZE; i++){
+			for(int j=0; j<SIZE; j++){
+				if(i <= 7 && j <= 7)
+					dist[i][j] = 14 - j - i;
+				else if(i <= 7)
+					dist[i][j] = j - i - 1;
+				else if(j <= 7)
+					dist[i][j] = i - j - 1;
+				else
+					dist[i][j] = i + j - 16;
+				//cout<< (int)dist[i][j] << ' ';
+			}
+			//cout << endl;
+		}
+	}
+
+	void runFloodfill(unsigned x, unsigned y){
+		//x = col, y = row
+		stack<coord> costack;
+		bool processed[SIZE][SIZE];
+		memset(processed, false, 256);
+		coord cur(x, y);
+		costack.push(cur);
+		while(!costack.empty()){
+			cur = costack.top();
+			costack.pop();
+			unsigned curx = cur.x;
+			unsigned cury = cur.y;  
+			processed[SIZE-1-cury][curx] = true;
+			if(dist[SIZE-1-cury][curx] == 0)
+				continue;
+			unsigned shortest = UINT_MAX;
+			//for all directions NSEW
+
+			//north
+			//coord n(x, y+1);
+			if(cury < SIZE-1 && !walls[SIZE-1-cury][curx].north){
+				if(dist[SIZE-1-cury-1][curx] < shortest)
+					shortest = dist[SIZE-1-cury-1][curx];
+				if(!processed[SIZE-1-cury-1][curx]){
+					coord n(curx, cury+1);
+					costack.push(n);
+				}
+			}
+			//south
+			if(cury > 0 && !walls[SIZE-1-cury][curx].south){
+				if(dist[SIZE-1-cury+1][curx] < shortest)
+					shortest = dist[SIZE-1-cury+1][curx];
+				if(!processed[SIZE-1-cury+1][curx]){
+					coord s(curx, cury-1);
+					costack.push(s);
+				}
+			}
+			//east			
+			if(curx < SIZE-1 && !walls[SIZE-1-cury][curx].east){
+				if(dist[SIZE-1-cury][curx+1] < shortest)
+					shortest = dist[SIZE-1-cury][curx+1];
+				if(!processed[SIZE-1-cury][curx+1]){
+					coord e(curx+1, cury);
+					costack.push(e);
+				}
+			}
+			//west			
+			if(curx > 0 && !walls[SIZE-1-cury][curx].west){
+				if(dist[SIZE-1-cury][curx-1] < shortest)
+					shortest = dist[SIZE-1-cury][curx-1];
+				if(!processed[SIZE-1-cury][curx-1]){
+					coord w(curx-1, cury);
+					costack.push(w);
+				}
+			}
+
+			if(shortest == UINT_MAX)
+				continue;
+			if(dist[SIZE-1-cury][curx] == shortest+1)
+				continue;
+			dist[SIZE-1-cury][curx] = shortest+1;
+			//push all neighbors on costack
+			coord neighbor(0, 0);
+
+			//TODO
+			//north neighbor
+			if(cury != 15){
+				neighbor.x = curx;
+				neighbor.y = cury+1;
+				costack.push(neighbor);
+			}
+			//south neighbor
+			if(cury != 0){
+				neighbor.x = curx;
+				neighbor.y = cury-1;
+				costack.push(neighbor);
+			}
+			//east neighbor
+			if(curx != 15){
+				neighbor.x = curx+1;
+				neighbor.y = cury;
+				costack.push(neighbor);
+			}
+			//west neighbor
+			if(curx != 0){
+				neighbor.x = curx-1;
+				neighbor.y = cury;
+				costack.push(neighbor);
+			}			
+		}
+		printArray(dist);
+	}
+
+
+	MouseMovement nextMovement(unsigned x, unsigned y, const Maze &maze) {
+		//cout << "next movement\n"; 
+		const bool frontWall = maze.wallInFront();
+		const bool leftWall  = maze.wallOnLeft();
+		const bool rightWall  = maze.wallOnRight();
+		updateWall(x, y, orient, frontWall, leftWall, rightWall);
+
+		// Pause at each cell if the user requests it.
+		// It allows for better viewing on command line.
+		if(pause) {
+			std::cout << "Hit enter to continue..." << std::endl;
+			std::cin.ignore(10000, '\n');
+			std::cin.clear();
+		}
+
+		std::cout << maze.draw(5) << std::endl << std::endl;
+
+		// If we somehow miraculously hit the center
+		// of the maze, just terminate and celebrate!
+		if(isAtCenter(x, y)) {
+			std::cout << "Found center! Good enough for the demo, won't try to get back." << std::endl;
+			return Finish;
+		}
+		
+		unsigned char cur = dist[SIZE-1-y][x];
+        //deadend, turn 180
+        if (frontWall && leftWall && rightWall){			
+            orient = (orient + 2) % 4;
+			runFloodfill(x, y);
+            return TurnAround;
+        }
+     
+        //determine lowest weight and direction 
+        unsigned char lowest = cur;
+        unsigned char east, west, north, south; 
+        east = west = north = south = UCHAR_MAX;
+        char dir = 'c';    //c for cur  
+		//EAST
+        if((x < SIZE-1) && !walls[SIZE-1-y][x].east)
+            east = dist[SIZE-1-y][x+1];
+        if(lowest > east){
+            lowest = east;
+            dir = 'e';
+        }
+		//WEST
+        if((x > 0) && !walls[SIZE-1-y][x].west)
+            west = dist[SIZE-1-y][x-1];       
+        if(lowest > west){
+            lowest = west;
+            dir = 'w';
+        }
+		//SOUTH
+        if((y > 0) && !walls[SIZE-1-y][x].south)
+            south = dist[SIZE-1-y+1][x];
+        if(lowest > south){
+            lowest = south;
+            dir = 's';
+        }
+		//NORTH
+        if((y < SIZE-1) && !walls[SIZE-1-y][x].north)    
+            north = dist[SIZE-1-y-1][x];
+        if(lowest > north){
+           lowest = north;
+           dir = 'n';
+        }              
+        //Check which direction to move next
+        if (dir == 'c'){
+			cout << "about to call floodfill bc stuck!\n";
+            runFloodfill(x, y);
+            return Stay;
+        }        
+        else if (orient == N){   
+			switch(dir){
+				 case 'n':
+					 orient = N;
+					 return MoveForward;
+					 break;
+				 case 's':
+					 orient = S;
+					 return TurnAround;
+					 break;
+				 case 'e':
+					 orient = E;
+					 return TurnClockwise;
+					 break;    
+				 case 'w':
+					 orient = W;
+					 return TurnCounterClockwise;
+					 break;
+				 default:
+					 return Stay; 
+					 break;
+			}                
+        }
+        else if (orient == E){
+			switch(dir){
+				 case 'n':
+					 orient = N;
+					 return TurnCounterClockwise;
+					 break;
+				 case 's':
+					 orient = S;
+					 return TurnClockwise;
+					 break;
+				 case 'e':
+					 orient = E;
+					 return MoveForward;
+					 break;    
+				 case 'w':
+					 orient = W;
+					 return TurnAround;
+					 break;
+				 default:
+					 return Stay; 
+					 break;
+			}
+        }
+        else if (orient == W){
+            switch(dir){
+				 case 'n':
+					 orient = N;
+					 return TurnClockwise;
+					 break;
+				 case 's':
+					 orient = S;
+					 return TurnCounterClockwise;
+					 break;
+				 case 'e':
+					 orient = E;
+					 return TurnAround;
+					 break;    
+				 case 'w':
+					 orient = W;
+					 return MoveForward;
+					 break;
+				 default:
+					 return Stay; 
+					 break;
+			}
+        }
+        else if (orient == S){
+            switch(dir){
+				 case 'n':
+					 orient = N;
+					 return TurnAround;
+					 break;
+				 case 's':
+					 orient = S;
+					 return MoveForward;
+					 break;
+				 case 'e':
+					 orient = E;
+					 return TurnCounterClockwise;
+					 break;    
+				 case 'w':
+					 orient = W;
+					 return TurnClockwise;
+					 break;
+				 default:
+					 return Stay; 
+					 break;
+			}
+        }
+		
+	}
+
+protected:
+	// Helps us determine that we should go forward if we have just turned left.
+	bool shouldGoForward;
+
+	// Helps us determine if we've made a loop around the maze without finding the center.
+	bool visitedStart;
+
+	// Indicates we should pause before moving to next cell.
+	// Useful for command line usage.
+	const bool pause;
+
+	bool isAtCenter(unsigned x, unsigned y) const {
+		unsigned midpoint = MazeDefinitions::MAZE_LEN / 2;
+
+		if(MazeDefinitions::MAZE_LEN % 2 != 0) {
+			return x == midpoint && y == midpoint;
+		}
+
+		return  (x == midpoint     && y == midpoint    ) ||
+			(x == midpoint - 1 && y == midpoint    ) ||
+			(x == midpoint     && y == midpoint - 1) ||
+			(x == midpoint - 1 && y == midpoint - 1);
+	}
+
+	void updateWall(unsigned x, unsigned y, int heading, bool frontWall, bool leftWall, bool rightWall){
+		//NORTH
+		if (heading == N){
+			if(frontWall){
+				walls[SIZE-1-y][x].north = true;
+				if (y < SIZE-1)
+					walls[SIZE-1-y-1][x].south = true;
+			} 
+			if(leftWall){
+				walls[SIZE-1-y][x].west = true;
+				if (x > 0)
+					walls[SIZE-1-y][x-1].east = true; 
+			} 
+			if(rightWall){
+				walls[SIZE-1-y][x].east = true;
+				if (x < SIZE-1)
+					walls[SIZE-1-y][x+1].west = true; 
+			}  
+		}
+		//EAST
+		else if (heading == E){
+			if(frontWall){
+				walls[SIZE-1-y][x].east = true; 
+				if(x<SIZE-1)
+					walls[SIZE-1-y][x+1].west = true;
+			}
+			if(leftWall){
+				walls[SIZE-1-y][x].north = true; 
+				if(y<SIZE-1)
+					walls[SIZE-1-y-1][x].south = true;
+			}
+			if(rightWall) {
+				walls[SIZE-1-y][x].south = true;
+				if(y>0)
+					walls[SIZE-1-y+1][x].north = true;
+			}
+		}
+		// WEST
+		else if (heading == W){
+			if(frontWall){
+				walls[SIZE-1-y][x].west = true;
+				if(x>0)
+					walls[SIZE-1][x-1].east = true;  
+			}
+			if(leftWall){
+				walls[SIZE-1-y][x].south = true;
+				if(y>0){
+					walls[SIZE-1-y+1][x].north = true;
+				}
+				if(rightWall){
+					walls[SIZE-1-y][x].north = true;
+					if(y<SIZE-1)
+						walls[SIZE-1-y-1][x].south = true;
+				}
+			}
+		}
+		//SOUTH
+		else{
+			if(frontWall){
+				walls[SIZE-1-y][x].south = true;
+				if(y>0)
+					walls[SIZE-1-y+1][x].north = true;
+			}
+			if(leftWall){
+				walls[SIZE-1-y][x].east = true;
+				if(x<SIZE-1)
+					walls[SIZE-1-y][x+1].west = true;
+			}       
+			if(rightWall) {
+				walls[SIZE-1-y][x].west = true;
+				if(x>0)
+					walls[SIZE-1-y][x-1].east = true;
+			}
+		}
+	}
+};
+
+int main(int argc, char * argv[]) {
+	MazeDefinitions::MazeEncodingName mazeName = MazeDefinitions::MAZE_CAMM_2012;
+	bool pause = true;
+
+	// Since Windows does not support getopt directly, we will
+	// have to parse the command line arguments ourselves.
+
+	// Skip the program name, start with argument index 1
+	for(int i = 1; i < argc; i++) {
+		if(strcmp(argv[i], "-m") == 0 && i+1 < argc) {
+			int mazeOption = atoi(argv[++i]);
+			if(mazeOption < MazeDefinitions::MAZE_NAME_MAX && mazeOption > 0) {
+				mazeName = (MazeDefinitions::MazeEncodingName)mazeOption;
+			}
+		} else if(strcmp(argv[i], "-p") == 0) {
+			pause = true;
+		} else {
+			std::cout << "Usage: " << argv[0] << " [-m N] [-p]" << std::endl;
+			std::cout << "\t-m N will load the maze corresponding to N, or 0 if invalid N or missing option" << std::endl;
+			std::cout << "\t-p will wait for a newline in between cell traversals" << std::endl;
+			return -1;
+		}
+	}
+
+	//LeftWallFollower leftWallFollower(pause);
+	Floodfill floodfill(pause);
+	Maze maze(mazeName, &floodfill);
+	std::cout << maze.draw(5) << std::endl << std::endl;
+
+	maze.start();
+}
